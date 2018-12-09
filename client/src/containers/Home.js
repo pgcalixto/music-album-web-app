09af44d879ccbda940c6615bd55a014461d03afc
Pedro Mendonça <pgcalixto@gmail.com>
2018-12-09 19:19:52 -0200

    Remove obsolete packages inclusion

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
-import { Link } from "react-router-dom";
 import Albums from '../components/Albums';
 import Collections from '../components/Collections';
 
